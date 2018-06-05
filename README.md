25136df03270ef29c4c7035b3839ca06d09ede12
yancyzhou <python@vip.126.com>
2018-06-06 00:25:41 +0800

    update

---
+++ README.md
@@ -0,0 +1,49 @@
+SanicRedis
+==============
+Redis support for sanic.
+
+Built on top of [aioredis](https://github.com/aio-libs/aioredis).
+
+Installation
+------------
+
+You can install this package as usual with pip:
+
+    pip install SanicRedis
+
+Example
+
+```python
+from sanic import Sanic
+from sanic.response import text,json
+from SanicRedis import SanicRedis
+
+app = Sanic(__name__)
+redisdb = dict(redisdb=8,
+    redishost='localhost',
+    redisport=6379,
+    redispwd='password'
+    )
+
+app.config.update(redisdb)
+BaseRedis.init_app(app)
+
+# group key of redis
+class GroupKey(SanicRedis):
+    __coll__ = 'GroupKey_' # GroupKey + key
+
+@app.route('/test1')
+async def test1(request):
+    await GroupKey.set('testkey','value')# key is GroupKey_testkey
+    result = await GroupKey.get('testkey')  # get value of GroupKey_testkey
+    return text(result)
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+```
+
+Resources
+---------
+
+- [PyPI](https://pypi.python.org/pypi/sanic-redis)
--- setup.py
+++ setup.py
@@ -3,7 +3,9 @@
 
 
 from __future__ import with_statement
-
+from setuptools import find_packages
+import ssl
+ssl._create_default_https_context=ssl._create_unverified_context
 import sys
 if sys.version_info < (2, 5):
     sys.exit('Python 2.5 or greater is required.')
@@ -12,26 +14,29 @@ try:
     from setuptools import setup
 except ImportError:
     from distutils.core import setup
+import SanicRedis.BaseRedis as BaseRedis
 
-import BaseRedis
 
-
-with open('README.rst') as fp:
-    readme = fp.read()
-
-with open('LICENSE') as fp:
-    license = fp.read()
-
-setup(name='sanic_redis',
+setup(name='SanicRedis',
       version=BaseRedis.__version__,
       description='Simple xml parse and build lib.',
-      long_description=readme,
+      long_description="SanicRedis is a sanic framework extension which adds support for the redis.",
       author='zhou biao',
       author_email='vincent321x@gmail.com',
       maintainer='zhou biao',
       maintainer_email='vincent321x@gmail.com',
-      url='https://github.com/heronotears/lazyxml',
-      packages=['sanic_redis'],
-      license=license,
-      platforms=['any'],
-      classifiers=[])
+      url='https://github.com/yancyzhou/sanic_redis',
+      install_requires=['redis'],
+      packages=find_packages(),
+      platforms=['all'],
+      classifiers=[
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: Implementation',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Software Development :: Libraries'
+      ])
